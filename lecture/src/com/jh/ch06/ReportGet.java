1ec800048beb530a7ba58d22faaa726fcafbc6ac
admin <admin@DESKTOP-BPP850B>
2021-01-06 14:39:40 +0900

    test second

---
+++ lecture/src/com/jh/ch06/ReportGet.java
@@ -0,0 +1,30 @@
+package com.jh.ch06;
+
+public class ReportGet {
+
+	
+	public void naver (int get) {
+		
+		int i = 0;
+		
+		if (get == i ) {
+		System.out.println("Stop");
+		}else {
+			System.out.println("Keep going");
+		}
+				
+		
+		
+	}
+	
+	public static void main(String[] args) {
+
+		int a = 9;
+		
+		ReportGet reportGet = new ReportGet();
+ 		
+		
+		reportGet.naver(3);
+	}
+
+}
